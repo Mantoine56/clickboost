425cff66a10f4af473e5a308a9dc5c6af8cbaf4c
Mantoine56 <aelias1988@hotmail.com>
2024-10-06 16:33:03 -0400

    V1.01

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
   },
   images: { unoptimized: true },
   trailingSlash: true,
-  env: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-  },
 };
 
 module.exports = nextConfig;
